922af8659f9585f17842313a4fcea6aaac6e983d
PawelBrzezinski <ppaul1989@gmail.com>
2016-03-14 22:10:42 +0000

    Changed views to blank pages

---
+++ FM/MainMenu.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// MainMenu.xaml.h
+// Declaration of the MainMenu class
+//
+
+#pragma once
+
+#include "MainMenu.g.h"
+
+namespace FM
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class MainMenu sealed
+	{
+	public:
+		MainMenu();
+	};
+}
---
+++ FM/MatchView.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// MatchView.xaml.h
+// Declaration of the MatchView class
+//
+
+#pragma once
+
+#include "MatchView.g.h"
+
+namespace FM
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class MatchView sealed
+	{
+	public:
+		MatchView();
+	};
+}
